9f4bdba0899735b15df21db9c8830793f3734e69
NLIPPI <nlippilira@gmail.com>
2026-01-28 03:30:09 -0300

    signals

--- sitio/apps.py
+++ sitio/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class SitioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sitio'
+from django.apps import AppConfig
+
+class SitioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sitio'
+
+    def ready(self):
+        import sitio.signals
